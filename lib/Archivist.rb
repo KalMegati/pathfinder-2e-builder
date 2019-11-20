8dc367a1006912f750518e5bea3899e43ce65a12
kalmegati <kalmegati@gmail.com>
2019-11-20 18:15:37 -0500

    Beginnings of stat progression for build choices

--- lib/Archivist.rb
+++ lib/Archivist.rb
@@ -7,7 +7,13 @@ class Archivist
   attr_reader :ancestries, :backgrounds, :classes
   
   @@source = "https://2e.aonprd.com/"
+
+  @@ance_parser = /Ability Boosts(Str.+|Dex.+|Con.+|Int.+|Wis.+|Cha.+)(Str.+|Dex.+|Con.+|Int.+|Wis.+|Cha.+)FreeAbility Flaw\(s\)(Str.+|Dex.+|Con.+|Int.+|Wis.+|Cha.+)Languages/
   
+  @@back_parser = /be to (Str.+|Dex.+|Con.+|Int.+|Wis.+|Cha.+) or (Str.+|Dex.+|Con.+|Int.+|Wis.+|Cha.+), and one/
+
+  @@clas_parser = /(STR\w+|DEX\w+|CON\w+|INT\w+|WIS\w+|CHA\w+)/
+
   def page_finder(aspect)
     Nokogiri::HTML(open(@@source+aspect.to_s.capitalize+'.aspx'))
   end
@@ -31,12 +37,30 @@ class Archivist
   def summarize(option, aspect) # => grabs flavor blurb
     page = "https://2e.aonprd.com/"+self.send("#{aspect}")[option]
     blurb = Nokogiri::HTML(open(page))
-    #binding.pry
     if page.include?("Backgrounds")
       puts blurb.text.match(/pg. 6\d(.+)Choose/)[1]
     else
       puts blurb.css("i")[1].text
     end
+  end
+
+  def ance_stats(option, aspect) # => grabs stats from ancestry
+    page = "https://2e.aonprd.com/"+self.send("#{aspect}")[option]
+    blurb = Nokogiri::HTML(open(page))
+    blurb.text.match(@@ance_parser)
+  end
+
+  def back_stats(option, aspect) # => grabs stats from background
+    page = "https://2e.aonprd.com/"+self.send("#{aspect}")[option]
+    blurb = Nokogiri::HTML(open(page))
+    blurb.text.match(@@back_parser)
+  end
+
+  def clas_stats(option, aspect) # => grabs stats from class
+    page = "https://2e.aonprd.com/"+self.send("#{aspect}")[option]
+    blurb = Nokogiri::HTML(open(page))
+    binding.pry
+    blurb.css("b")[2].text.match(@@clas_parser)
   end
 
   def confirm(option, aspect)
@@ -45,3 +69,7 @@ class Archivist
   end  
 
 end
+
+#Archivist.new.ance_stats("Dwarf", :ancestries)
+
+#binding.pry
--- lib/Builder.rb
+++ lib/Builder.rb
@@ -2,7 +2,7 @@ require_relative 'Archivist.rb'
 
 class Builder
   
-  attr_accessor :name, :ancestries, :backgrounds, :classes, :description
+  attr_accessor :name, :ancestries, :backgrounds, :classes, :description, :statistics
   
   @@all = []
   
@@ -10,6 +10,14 @@ class Builder
     @name = name
     @description = ""
     @@all << self
+    @statistics = {
+      str: 10,
+      dex: 10,
+      con: 10,
+      int: 10,
+      wis: 10,
+      cha: 10
+    }
   end
 
   def writer
